49d9567a820fdfb7f4661244fed85ffc6deb093d
Lucy Davinhart <lucy@lucymhdavies.com>
2023-03-14 16:49:05 +0000

    create a single resource, as a test

--- split/compute/main.tf
+++ split/compute/main.tf
@@ -31,3 +31,18 @@ data "tfe_outputs" "network" {
 output "upstream" {
   value = data.tfe_outputs.network.nonsensitive_values
 }
+
+locals {
+  resource_group = data.tfe_outputs.network.nonsensitive_values.resource_group
+}
+
+
+# TODO: all of this
+# https://github.com/FancyCorp-Demo/terraform-azure-webserver/blob/main/main.tf#L36-L145
+
+resource "azurerm_public_ip" "example" {
+  name                = "strawbtest-public-ip"
+  resource_group_name = local.resource_group.name
+  location            = local.resource_group.location
+  allocation_method   = "Static"
+}
